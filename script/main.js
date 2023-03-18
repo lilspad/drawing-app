0d28bcf9776c7047156f1e88147cbbe87c98dfb2
lilspad <lilypaczesniak@outlook.com>
2023-03-18 00:44:41 +0000

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,1 +1,26 @@
-import '../style/style.css';
+import '../style/style.css'
+
+const container = document.getElementById("canvas-container");
+const canvas = document.getElementById("canvas");
+const width = 1920;
+const height = 1080;
+
+// context of the canvas
+const context = canvas.getContext("2d");
+context.imageSmoothingEnabled = true;
+
+// resize canvas
+canvas.height = height;
+canvas.width = width;
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect(), 
+    scaleX = canvas.width / rect.width, 
+    scaleY = canvas.height / rect.height;
+
+  return {
+    x: (evt.clientX - rect.left) * scaleX,
+    y: (evt.clientY - rect.top) * scaleY
+  }
+}
+

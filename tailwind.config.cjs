90a776f64e8e7d152d1e8fbd4f9c1f6d33b0d058
lilspad <lilypaczesniak@outlook.com>
2023-03-21 00:33:46 +0000

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html"
-  ],
+  content: ['./**/*.{html,js}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-  variants: {
-    extend: {
-        display: ["group-hover"],
-    },
-}
+  plugins: []
 }
 

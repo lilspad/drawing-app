e0a24b444988b8c726ae3bc6e1b356771f71c9a9
lilspad <lilypaczesniak@outlook.com>
2023-03-21 00:42:44 +0000

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: {
+    content: ['./**/*.html'],
+    safelist: [
+      'bg-red-500',
+      'bg-orange-500',
+      'bg-yellow-500',
+      'bg-green-500',
+      'bg-blue-500',
+      'bg-violet-500'
+    ]
+  },
   content: ['./**/*.{html,js}'],
   theme: {
     extend: {},
